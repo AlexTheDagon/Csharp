f5cfe01db0725b0c3773cac756396a0442c6d7ae
AlexTheDagon <bogdantudoralex@yahoo.com>
2021-12-20 17:02:22 +0200

    dfgsdfgsd

--- MedicalAppointments/Startup.cs
+++ MedicalAppointments/Startup.cs
@@ -8,7 +8,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+/// <summary>
+/// YEEET
+/// </summary>
 namespace MedicalAppointments
 {
     public class Startup
