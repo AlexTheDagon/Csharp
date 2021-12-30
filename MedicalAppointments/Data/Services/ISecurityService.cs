5817c95d3453cd15e5b74e71bac2d3d3ce661501
Tudor-Alexandru Bogdan <bogdantudoralex@yahoo.com>
2021-12-30 19:44:55 +0200

    Delete MedicalAppointments directory

--- MedicalAppointments/Data/Services/ISecurityService.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MedicalAppointments.Data.Services
-{
-    public interface ISecurityService
-    {
-        public string HashPassword(string password);
-        public bool VerifyPassword(string password, string passwordHash);
-    }
-}
